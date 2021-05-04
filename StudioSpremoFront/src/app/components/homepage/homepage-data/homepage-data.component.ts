9c0717a9bd2dca6ce24dc67dc8438e44d9e202c6
Natasa Spremo <natasa-spremo@hotmail.com>
2021-05-04 18:34:13 +0200

    homepage i contact us sredjeni

--- StudioSpremoFront/src/app/components/homepage/homepage-data/homepage-data.component.ts
+++ StudioSpremoFront/src/app/components/homepage/homepage-data/homepage-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage-data',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage-data.component.css']
 })
 export class HomepageDataComponent implements OnInit {
-
+  name: string = "";
+  email: string = "";
+  testMessage: string = "";
+  phone: string = "";
+  reactiveForm: FormGroup = new FormGroup({
+    'name': new FormControl('', [Validators.required]),
+    'email': new FormControl('', [Validators.required, Validators.email]),
+    'phone': new FormControl(''),
+    'testMessage': new FormControl('', [Validators.required, Validators.maxLength(100)])
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    
+  }
 }
